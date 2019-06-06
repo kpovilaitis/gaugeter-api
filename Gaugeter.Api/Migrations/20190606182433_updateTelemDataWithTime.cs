8ec2dee76d154abf4c490861c4c25f130f238f5f
kestutispovilaitis <kiesiusp@gmail.com>
2019-06-06 21:36:06 +0300

    added migration

---
+++ Gaugeter.Api/Migrations/20190606182433_updateTelemDataWithTime.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Gaugeter.Api.Migrations
+{
+    public partial class updateTelemDataWithTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TelemData_Job_JobId",
+                table: "TelemData");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "JobId",
+                table: "TelemData",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "DateCreated",
+                table: "TelemData",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TelemData_Job_JobId",
+                table: "TelemData",
+                column: "JobId",
+                principalTable: "Job",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TelemData_Job_JobId",
+                table: "TelemData");
+
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "TelemData");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "JobId",
+                table: "TelemData",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TelemData_Job_JobId",
+                table: "TelemData",
+                column: "JobId",
+                principalTable: "Job",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
